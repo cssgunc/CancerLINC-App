2cfb35bcce25a046b4a8d436e0bccfbff608991b
Hamzah Yousuf <hamyou@ad.unc.edu>
2026-02-17 02:10:33 -0500

    Profile page rework -- Ted and Hamzah

--- app/routes.ts
+++ app/routes.ts
@@ -3,6 +3,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
     index("routes/login.tsx"),
     route("_index", "routes/_index.tsx"),
+    route("profile", "routes/profile.tsx"),
     route("forgot-pass", "routes/forgot_pass.tsx"),
     route("verify-email", "routes/verify_email.tsx"),
     route("change-password", "routes/change_password.tsx"),
