577db580070b61a2bb67305f7adcb6385c909499
Arya Venkatesan <ar6165@gmail.com>
2026-03-04 16:24:51 -0500

    fix: flutter build fixes

--- cancerlinc/lib/main.dart
+++ cancerlinc/lib/main.dart
@@ -6,7 +6,12 @@ import 'package:cancerlinc/pages/login_page.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  if (Firebase.apps.isEmpty) {
+    await Firebase.initializeApp(
+      name: "Cancerlinc",
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
 
   runApp(const MyApp());
 }
